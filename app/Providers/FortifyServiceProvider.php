098d78fcac382c1a37394d77def6076db6372814
wdocker2 <wdocker2@wdocker2.com>
2023-02-19 15:55:18 -0300

    new user email verification

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,6 +41,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView('auth.register');
         Fortify::requestPasswordResetLinkView('auth.forgot-password');
         Fortify::resetPasswordView('auth.reset-password');
+        Fortify::verifyEmailView('auth.verify-email');
 
         //custom login with return $user, normal way return to Home
 /*         Fortify::authenticateUsing(function(Request $request) {
---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,3 @@
+<div style="margin-bottom: 0.5rem; font-size: 0.875rem; color: #4a5568;">
+    <p>Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.</p>
+</div>
