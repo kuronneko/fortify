3d8d84fe374e268f157a7cae88bef6df342a3e88
wdocker2 <wdocker2@wdocker2.com>
2023-02-19 16:04:55 -0300

    resend user verification email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,3 +1,13 @@
-<div style="margin-bottom: 0.5rem; font-size: 0.875rem; color: #4a5568;">
-    <p>Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.</p>
+<div>
+    <p>Thanks for signing up! Before getting started, please verify your email address by clicking on the link we just
+        emailed to you. If you didn't receive the email, you can request another one by clicking the button below:</p>
+    <form method="POST" action="/email/verification-notification">
+        @csrf
+        <button type="submit">Resend Verification Link</button>
+    </form>
+    @if (session('status') == 'verification-link-sent')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            A new email verification link has been emailed to you!
+        </div>
+    @endif
 </div>
